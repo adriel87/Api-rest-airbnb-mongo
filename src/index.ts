eff531418f39d7e2a4997e32155ff952930cbaab
adriel <adrielarochaoronoz@gmail.com>
2023-09-08 17:31:31 +0100

    create basic configuration in express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hola mundo');
+import { createRestApi, envConstant } from "#core/index.ts";
+
+
+
+
+const restApiServer = createRestApi();
+
+restApiServer.listen(envConstant.PORT,()=>{
+
+    console.log(`Running on port ${envConstant.PORT}`);
+    
+
+})
